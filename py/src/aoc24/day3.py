6c7538e0894c30212544adf7c3d3bcc109bdcc6f
Arslan <rslnkrmt2552@gmail.com>
2024-12-05 11:27:53 +0100

    regex based approach

--- py/src/aoc24/day3.py
+++ py/src/aoc24/day3.py
@@ -2,19 +2,20 @@ from pathlib import Path
 from aoc24 import utils
 import re
 
-MULRE = re.compile(r"mul\((\d+)\,(\d+)\)")
+MULRE = re.compile(r"(don\'t\(\))|(do\(\))|(mul\((\d+)\,(\d+)\))")
 
-# print(
-#     MULRE.findall(
-#         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-#     )
-# )
+print(
+    MULRE.findall(
+        "xdon't()mul(2,4)do()%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    )
+)
 
 
 def read_data(pth: Path):
     lines = pth.read_text().splitlines()
 
     group_gen = (match.groups() for line in lines for match in MULRE.finditer(line))
+    print(list(group_gen))
     tuple_gen = ((int(a), int(b)) for (a, b) in group_gen)
 
     to_mul: list[tuple[int, int]] = list(tuple_gen)
